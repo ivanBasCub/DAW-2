18d29934483e5ff1d53cdf7668a8f3650250baf9
ivanBasCub <ibascub2017@hotmail.com>
2025-01-27 10:44:22 +0100

    lol

--- DWEC/06-Tema06/Ejemplos/React/posts/src/Post.jsx
+++ DWEC/06-Tema06/Ejemplos/React/posts/src/Post.jsx
@@ -2,16 +2,26 @@ import { useState, useEffect } from "react";
 
 export default function Posts(){
     const [posts, setPosts] = useState([]);
+    const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function fecthPosts() {
-            const res = await fetch("https://jsonplaceholder.typicode.com/posts")
-            const postList = await res.json()
+            const resPost = await fetch("https://jsonplaceholder.typicode.com/posts")
+            const postList = await resPost.json()
+
             setPosts(postList)
             setLoading(false)
         }
+
+        async function fecthUsers() {
+            const res = await fetch("https://jsonplaceholder.typicode.com/users");
+            const userList = await res.json()
+
+            setUsers(userList)
+        }
         fecthPosts()
+        fecthUsers()
     }, []) // La segunda condicion es cuando se modifica unas variables
 
     return (
@@ -19,6 +29,9 @@ export default function Posts(){
             {posts.map(post =>
                 <div className="post" key={post.id}>
                     <h1>{post.title}</h1>
+                    <p>
+                        {users[post.userId-1].username}
+                    </p>
                     <p>{post.body}</p>
                 </div>
             )}
